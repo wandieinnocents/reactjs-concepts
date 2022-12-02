53f983a944a9021d904c4fb3bc89f3d8db3a3b09
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-12-02 21:07:38 +0300

    Add class component

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+// class
+class Welcome extends Component{
+    // render
+    render(){
+    // return
+        return <h1>CLASS COMPONENT</h1>
+    }
+}
+
+export default Welcome;
