f047f469e6a54beff56023d153a6144bd32228e7
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-12-02 20:46:59 +0300

    Refactor code , add greet component

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// create function
+// function Greeet() {
+//     return <h1> Helo wandie function</h1>
+// }
+
+ const Greet = () =>  <h1>KAVUMA INNOCENT</h1>
+
+
+// export component
+export default Greet
