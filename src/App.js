d5ccf50bc249a9fbf2f6d2128cfdd31ddb122056
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-12-04 01:06:40 +0300

    Add jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <Hello />
      
     </div>
   );
---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    return(
+        <div>
+            <h1>HELLO WANDIE</h1>
+            <p>What is the d</p>
+        </div>
+    )
+}
+
+export default Hello;
