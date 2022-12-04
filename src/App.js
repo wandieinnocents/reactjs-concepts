be7eb396c2543d4f561a8061f11561812ad55896
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-12-04 23:42:11 +0300

    WOrking with states in class components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-      
-      
-      <Message /> 
-      
-      
-     
-    </div>
-  );
+class App extends Component  {
+  render(){
+    return (
+      <div className="App">
+        <Message /> 
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,11 +2,32 @@ import React,{Component} from 'react'
 
 // class
 class Message extends Component{
+
+    // create state object
+    constructor(){
+        super()
+        this.state = {
+            message:'welcome visitor'
+        }
+    }
+
+    // change message 
+    changeMessage(){
+        this.setState({
+            message: 'Thank you for subscribing'
+        })
+    }
+
+
+
     // render
     render(){
     // return
         return (
-            <h1>WELCOME VISITOR</h1>
+            <div>
+                <h1>{this.state.message}</h1>
+                <button  onClick={() => this.changeMessage() } >SUBSCRIBE</button>
+            </div>
         )
     }
 }
